b44bf7f3562d9384cfd3120e2dd6afa1046ce4d4
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-03-08 11:14:43 -0500

    initial commit

---
+++ sqlSyntax.sql
@@ -0,0 +1,7 @@
+SELECT name, year FROM movies WHERE year=1902 AND rank>5;
+
+SELECT name, year FROM movies WHERE year=1992;
+
+SELECT count(*) FROM movies WHERE year=1982;
+
+SELECT * FROM actors WHERE last_name like '%stack%';
