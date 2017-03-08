a9128328608c6b589a6c611efd568f9398a01403
Jodie Ly <jodie.ly@gmail.com>
2017-03-08 16:02:47 -0500

    either IMBD is stupid or we are?

--- sqlSyntax.sql
+++ sqlSyntax.sql
@@ -23,4 +23,16 @@ SELECT first_name, last_name FROM actors INNER JOIN roles ON actors.id = roles.a
 -- Film-Noir
 SELECT first_name, last_name, movies.name, movies.year FROM directors INNER JOIN movies_directors ON directors.id = movies_directors.director_id INNER JOIN movies ON movies_directors.movie_id = movies.id INNER JOIN movies_genres ON movies.id = movies_genres.movie_id WHERE movies_genres.genre = "Film-Noir" AND movies.year % 4 = 0 ORDER BY movies.name;
 
+SELECT first_name, last_name, movies.name FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON movies.id = roles.movie_id INNER JOIN movies_genres ON movies.id = movies_genres.movie_id WHERE movies_genres.genre = "Drama" AND first_name != "Kevin" AND last_name != "Bacon" AND roles.movie_id IN 
+        (SELECT movie_id FROM roles WHERE actor_id IN 
+            (SELECT id FROM actors WHERE first_name = "Kevin" AND last_name = "Bacon"));
 
+SELECT first_name, last_name, actors.id FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON movies.id = roles.movie_id WHERE movies.year < 1900 AND actors.id IN  
+    (SELECT actors.id FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON movies.id = roles.movie_id WHERE movies.year > 2000) 
+    GROUP BY first_name, last_name, actors.id;
+
+SELECT first_name, last_name, movies.name, movies.year, COUNT(DISTINCT roles.role) nOfRoles FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON movies.id = roles.movie_id WHERE movies.year > 1990 GROUP BY first_name, last_name, movies.name HAVING nOfRoles >= 5;
+
+SELECT movies.year, COUNT (*) nOfMovies FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON movies.id = roles.movie_id WHERE actors.gender = 'F' AND movies.id NOT IN  
+    (SELECT movies.id FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON movies.id = roles.movie_id WHERE actors.gender = 'M') 
+    GROUP BY movies.year;
