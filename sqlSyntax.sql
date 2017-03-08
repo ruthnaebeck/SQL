bb0e50a5e8a9ce8916819c06028aa7930c96e6f2
Jodie Ly <jodie.ly@gmail.com>
2017-03-08 12:16:31 -0500

    up to prolific

--- sqlSyntax.sql
+++ sqlSyntax.sql
@@ -5,3 +5,13 @@ SELECT name, year FROM movies WHERE year=1992;
 SELECT count(*) FROM movies WHERE year=1982;
 
 SELECT * FROM actors WHERE last_name like '%stack%';
+
+SELECT first_name, COUNT(*) c FROM actors GROUP BY first_name ORDER BY c;
+
+SELECT first_name, COUNT(*) c FROM actors GROUP BY first_name ORDER BY c DESC LIMIT 10;
+
+SELECT last_name, COUNT(*) c FROM actors GROUP BY last_name ORDER BY c DESC LIMIT 10;
+
+SELECT first_name, last_name, COUNT(*) c FROM actors GROUP BY first_name, last_name ORDER BY c DESC LIMIT 10;
+
+SELECT first_name, last_name, COUNT(*) role_count FROM actors INNER JOIN roles ON actors.id = roles.actor_id INNER JOIN movies ON roles.movie_id = movies.id GROUP BY first_name, last_name ORDER BY role_count DESC LIMIT 100;
